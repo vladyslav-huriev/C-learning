0d65cecc82ce432b20f23cfbe0c7925dc56ecd29
Vladyslav Huriev <vladyslav.guriev@gmail.com>
2024-12-04 17:53:09 +0100

    Add files via upload

---
+++ ДЗ 6.1.3 Цикли.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter the largest number to find prime numbers: ");
+int largestNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Prime numbers from 1 to " + largestNumber + " are:");
+for (int i = 2; i <= largestNumber; i++)
+{
+    bool isPrime = true;
+
+    for (int j = 2; j * j <= i; j++)
+    {
+        if (i % j == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime)
+    {
+        Console.WriteLine(i);
+    }
+}
