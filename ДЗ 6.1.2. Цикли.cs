6a1e83dbee9a9ab85d299cad4ee1b6c92a3075a8
Vladyslav Huriev <vladyslav.guriev@gmail.com>
2024-12-04 17:11:26 +0100

    Add files via upload

---
+++ ДЗ 6.1.2. Цикли.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Please enter the number of lines you want to see with stars: ");
+int rows = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= rows; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
