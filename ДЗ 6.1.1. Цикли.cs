142c4be9e3d0b0678b76f9d3d46a8f6de1f98bd8
Vladyslav Huriev <vladyslav.guriev@gmail.com>
2024-12-04 16:58:26 +0100

    Add files via upload

---
+++ ДЗ 6.1.1. Цикли.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.WriteLine("How many employees are in the company");
+int emloyees = int.Parse(Console.ReadLine());
+
+double salaryintotal = 0;
+
+for (int i = 1; i <= emloyees; i++)
+{
+    Console.WriteLine($"What is the salary of the employee #{i}: ");
+    salaryintotal += int.Parse(Console.ReadLine());
+}
+
+double avaragesalary = salaryintotal / emloyees;
+Console.WriteLine($"The avarage salary in the company is: {avaragesalary}");
