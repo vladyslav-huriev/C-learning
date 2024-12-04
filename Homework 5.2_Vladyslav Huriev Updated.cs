b0ab8b248d572b16e02493d774dac4e85d5729be
Vladyslav Huriev <vladyslav.guriev@gmail.com>
2024-12-04 13:52:31 +0100

    Add files via upload

---
+++ Homework 5.2_Vladyslav Huriev Updated.cs
@@ -0,0 +1,41 @@
+﻿try
+{
+    Console.WriteLine("Enter the first figure: ");
+    int numberfirst = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter the second figure: ");
+    int numbersecond = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter the activity what you whant to do with those figures (+, -, *, /): ");
+    string figures = Console.ReadLine();
+
+    try
+    {
+        switch (figures)
+        {
+            case "+":
+                Console.WriteLine($"The result of your calculation is: {numberfirst + numbersecond}");
+                break;
+            case "*":
+                Console.WriteLine($"The result of your calculation is: {numberfirst * numbersecond}");
+                break;
+            case "-":
+                Console.WriteLine($"The result of your calculation is: {numberfirst - numbersecond}");
+                break;
+            case "/":
+                Console.WriteLine($"The result of your calculation is: {numberfirst / numbersecond}");
+                break;
+            default:
+                Console.WriteLine("Error has occured.");
+                break;
+        }
+    }
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("Error has occurred - You cannot divide by 0.");
+    }
+}
+catch (FormatException)
+{
+    Console.WriteLine("Error has occurred - Please enter a number.");
+}
